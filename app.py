de1e0a3d2e2e781d763755c327b073f050e6893a
Simone Conti <contisimone4@gmail.com>
2024-09-27 11:41:44 +0200

    added new routes

--- app.py
+++ app.py
@@ -12,14 +12,50 @@ def add():
         return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 
 #Endpoint /sub for subtraction which takes a and b as query parameters.
-
+@app.route('/sub')
+def sub():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a-b), 200)
 #Endpoint /mul for multiplication which takes a and b as query parameters.
-
+@app.route('/mul')
+def mul():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a*b), 200)
 #Endpoint /div for division which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
-
+@app.route('/div')
+def div():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if not a or not b:
+        return make_response('Missing parameters, needs both a and b\n', 400)
+    if b == 0:
+        return make_response('Division by zero\n', 400)
+    return make_response(jsonify(s=a/b), 200)
 #Endpoint /mod for modulo which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
-
+@app.route('/mod')
+def mod():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if not a or not b:
+        return make_response('Missing parameters, needs both a and b\n', 400)
+    if b == 0:
+        return make_response('Division by zero\n', 400)
+    return make_response(jsonify(s=a%b), 200)
 #Endpoint /random which takes a and b as query parameters and returns a random number between a and b included. Returns HTTP 400 BAD REQUEST if a is greater than b.
+@app.route('/random')
+def random():
+    a = request.args.get('a', type=int)
+    b = request.args.get('b', type=int)
+    if a and b:
+        if a > b:
+            return make_response('a should be less than b\n', 400)
+        import random
+        return make_response(jsonify(s=random.randint(a, b)), 200)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
